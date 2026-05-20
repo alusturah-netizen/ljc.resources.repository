dfd4da9e61e754627bad3fcf08d1fb91d52dd7a9
alusturah-netizen <abiru2867@loyolajesuit.org>
2026-05-20 19:44:48 +0100

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // 🟢 Hardcoding the exact URL from your active Scholar Vault dashboard
-const supabaseUrl = 'https://hyvjdxnmfmwdhwnslndf.supabase.co';
+const supabaseUrl = 'https://eprmnghhatpfugjoplyu.supabase.co';
 
 // 🔍 PASTE YOUR ACTUAL SCHOLAR VAULT ANON KEY INSTEAD OF THE TEXT BELOW:
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwcm1uZ2hoYXRwZnVnam9wbHl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNjk3NjQsImV4cCI6MjA5NDg0NTc2NH0.bpzWfpluaTHys4TdhCpLZeIkfb-dPEaEaeZMHvQ5woY';
